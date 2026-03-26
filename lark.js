8c3e3fff579c813e331d1afd99f4490c22fe3371
yhzcake <1367808860@qq.com>
2026-03-26 23:45:05 +0800

    修复api调用错误

---
+++ lark.js
@@ -0,0 +1,500 @@
+logger.info(logger.yellow("- 正在加载飞书适配器插件"))
+
+import makeConfig from "../../lib/plugins/config.js"
+import * as lark from "@larksuiteoapi/node-sdk"
+
+const { config, configSave } = await makeConfig("Lark", {
+  tips: "",
+  permission: "master",
+  app_id: "",
+  app_secret: "",
+  encrypt_key: "",
+  verification_token: "",
+  webhook_url: "",
+  proxy: "",
+}, {
+  tips: [
+    "欢迎使用 TRSS-Yunzai Lark Plugin !",
+    "飞书官方机器人适配器",
+  ],
+})
+
+const adapter = new class LarkAdapter {
+  id = "Lark"
+  name = "LarkBot"
+  version = "v1.0.0"
+
+  async makeMsg(msg, client) {
+    if (!Array.isArray(msg))
+      msg = [msg]
+    
+    const content = { msg_type: "text", content: { text: "" } }
+    const files = []
+    
+    for (let i of msg) {
+      if (typeof i !== "object")
+        i = { type: "text", text: i }
+      
+      switch (i.type) {
+        case "text":
+          content.content.text += i.text
+          break
+        case "image":
+          content.msg_type = "image"
+          content.content.image_key = await this.uploadImage(i.file, client)
+          break
+        case "at":
+          if (i.qq === "all") {
+            content.content.text += `<at user_id="all">@all</at>`
+          } else {
+            content.content.text += `<at user_id="${i.qq.replace(/^lark_/, "")}"></at>`
+          }
+          break
+        case "reply":
+          content.content.text += `[回复：${i.id}]`
+          break
+        case "video":
+          content.msg_type = "video"
+          content.content.video_key = await this.uploadVideo(i.file, client)
+          break
+        case "file":
+          content.msg_type = "file"
+          content.content.file_key = await this.uploadFile(i.file, client)
+          break
+        default:
+          content.content.text += JSON.stringify(i)
+      }
+    }
+    
+    return { content, files }
+  }
+
+  async uploadImage(file, client) {
+    const fileData = await Bot.fileType({ file })
+    const ret = await client.im.file.create({
+      data: {
+        file_type: "image",
+        file_name: fileData.name,
+        file: fileData.buffer,
+      }
+    })
+    return ret.data?.file_key
+  }
+
+  async uploadVideo(file, client) {
+    const fileData = await Bot.fileType({ file })
+    const ret = await client.im.file.create({
+      data: {
+        file_type: "video",
+        file_name: fileData.name,
+        file: fileData.buffer,
+      }
+    })
+    return ret.data?.file_key
+  }
+
+  async uploadFile(file, client) {
+    const fileData = await Bot.fileType({ file })
+    const ret = await client.im.file.create({
+      data: {
+        file_type: "file",
+        file_name: fileData.name,
+        file: fileData.buffer,
+      }
+    })
+    return ret.data?.file_key
+  }
+
+  async sendMsg(data, msg) {
+    const { content } = await this.makeMsg(msg, data.bot)
+    Bot.makeLog("info", `发送消息：[${data.id}] ${JSON.stringify(content)}`, data.self_id)
+    
+    const ret = await data.bot.im.message.create({
+      params: {
+        receive_id_type: data.message_type === 'private' ? 'user_id' : 'chat_id',
+      },
+      data: {
+        receive_id: data.id,
+        msg_type: content.msg_type,
+        content: JSON.stringify(content.content),
+      }
+    })
+    
+    return { data: ret, message_id: ret.data?.message_id }
+  }
+
+  async sendFriendMsg(data, msg) {
+    const friendInfo = await this.getFriendInfo(data)
+    data.id = friendInfo.user_id.replace(/^lark_/, "")
+    data.message_type = 'private'
+    return this.sendMsg(data, msg)
+  }
+
+  async getMsg(data, message_id) {
+    const ret = await data.bot.im.message.get({
+      path: {
+        message_id: message_id,
+      }
+    })
+    return this.makeMessageArray(ret)
+  }
+
+  async getFriendMsg(data, message_id) {
+    data.id = (await this.getFriendInfo(data)).user_id.replace(/^lark_/, "")
+    return this.getMsg(data, message_id)
+  }
+
+  recallMsg(data, message_id) {
+    Bot.makeLog("info", `撤回消息：[${data.id}] ${message_id}`, data.self_id)
+    return data.bot.im.message.delete({
+      path: {
+        message_id: message_id,
+      }
+    })
+  }
+
+  async recallFriendMsg(data, message_id) {
+    data.id = (await this.getFriendInfo(data)).user_id.replace(/^lark_/, "")
+    return this.recallMsg(data, message_id)
+  }
+
+  async getFriendInfo(data) {
+    const ret = await data.bot.contact.user.get({
+      path: {
+        user_id: data.user_id.replace(/^lark_/, "")
+      }
+    })
+    return {
+      user_id: `lark_${ret.data?.user?.user_id}`,
+      nickname: ret.data?.user?.name,
+      avatar: ret.data?.user?.avatar_url,
+    }
+  }
+
+  getFriendArray(id) {
+    const array = []
+    if (Bot[id].userCache) {
+      for (const [user_id, user] of Bot[id].userCache)
+        array.push({
+          user_id: `lark_${user_id}`,
+          nickname: user.name,
+          avatar: user.avatar_url,
+        })
+    }
+    return array
+  }
+
+  getFriendList(id) {
+    const array = []
+    for (const { user_id } of this.getFriendArray(id))
+      array.push(user_id)
+    return array
+  }
+
+  getFriendMap(id) {
+    const map = new Map
+    for (const i of this.getFriendArray(id))
+      map.set(i.user_id, i)
+    return map
+  }
+
+  getGroupArray(id) {
+    const array = []
+    if (Bot[id].chatCache) {
+      for (const [chat_id, chat] of Bot[id].chatCache)
+        array.push({
+          group_id: `lark_${chat_id}`,
+          group_name: chat.name,
+        })
+    }
+    return array
+  }
+
+  getGroupList(id) {
+    const array = []
+    for (const { group_id } of this.getGroupArray(id))
+      array.push(group_id)
+    return array
+  }
+
+  getGroupMap(id) {
+    const map = new Map
+    for (const i of this.getGroupArray(id))
+      map.set(i.group_id, i)
+    return map
+  }
+
+  getGroupMemberMap(id) {
+    const map = new Map
+    for (const i of this.getGroupList(id))
+      map.set(i, new Map)
+    return map
+  }
+
+  pickFriend(id, user_id) {
+    if (typeof user_id !== "string")
+      user_id = String(user_id)
+    const i = {
+      ...Bot[id].fl.get(user_id),
+      self_id: id,
+      bot: Bot[id],
+      user_id: user_id.replace(/^lark_/, ""),
+    }
+    return {
+      ...i,
+      sendMsg: msg => this.sendFriendMsg(i, msg),
+      getMsg: message_id => this.getFriendMsg(i, message_id),
+      recallMsg: message_id => this.recallFriendMsg(i, message_id),
+      getInfo: () => this.getFriendInfo(i),
+      getAvatarUrl: async () => (await this.getFriendInfo(i)).avatar,
+    }
+  }
+
+  pickMember(id, group_id, user_id) {
+    if (typeof group_id !== "string")
+      group_id = String(group_id)
+    if (typeof user_id !== "string")
+      user_id = String(user_id)
+    const i = {
+      ...Bot[id].fl.get(user_id),
+      self_id: id,
+      bot: Bot[id],
+      group_id: group_id.replace(/^lark_/, ""),
+      user_id: user_id.replace(/^lark_/, ""),
+    }
+    return {
+      ...this.pickFriend(id, user_id),
+      ...i,
+    }
+  }
+
+  pickGroup(id, group_id) {
+    if (typeof group_id !== "string")
+      group_id = String(group_id)
+    const i = {
+      ...Bot[id].gl.get(group_id),
+      self_id: id,
+      bot: Bot[id],
+      id: group_id.replace(/^lark_/, ""),
+    }
+    return {
+      ...i,
+      sendMsg: msg => this.sendMsg(i, msg),
+      getMsg: message_id => this.getMsg(i, message_id),
+      recallMsg: message_id => this.recallMsg(i, message_id),
+      getAvatarUrl: () => i.avatar,
+      pickMember: user_id => this.pickMember(id, i.id, user_id),
+    }
+  }
+
+  makeMessageArray(data) {
+    const messageData = data.data || data
+    messageData.user_id = `lark_${messageData.sender?.id || messageData.sender_id}`
+    messageData.sender = {
+      user_id: messageData.user_id,
+      nickname: messageData.sender?.name || messageData.sender?.name,
+      avatar: messageData.sender?.avatar_url || messageData.sender?.avatar_url,
+    }
+    messageData.message_id = messageData.message_id || messageData.msg_id
+
+    messageData.message = []
+    messageData.raw_message = ""
+
+    const content = typeof messageData.content === 'string' ? JSON.parse(messageData.content) : messageData.content
+
+    if (content?.text) {
+      messageData.message.push({ type: "text", text: content.text })
+      messageData.raw_message += content.text
+    }
+
+    if (content?.mentions) {
+      for (const mention of content.mentions) {
+        messageData.message.push({ 
+          type: "at", 
+          qq: `lark_${mention.id}` 
+        })
+        messageData.raw_message += `[提及：lark_${mention.id}]`
+      }
+    }
+
+    if (content?.image_key) {
+      messageData.message.push({
+        type: "image",
+        file: content.image_key,
+      })
+      messageData.raw_message += `[图片：${content.image_key}]`
+    }
+
+    if (content?.video_key) {
+      messageData.message.push({
+        type: "video",
+        file: content.video_key,
+      })
+      messageData.raw_message += `[视频：${content.video_key}]`
+    }
+
+    if (content?.file_key) {
+      messageData.message.push({
+        type: "file",
+        file: content.file_key,
+      })
+      messageData.raw_message += `[文件：${content.file_key}]`
+    }
+
+    return messageData
+  }
+
+  makeMessage(data) {
+    data.post_type = "message"
+    data = this.makeMessageArray(data)
+    if (data.user_id === data.self_id) return
+
+    const eventData = data.data || data
+    if (eventData.chat_type === "group") {
+      data.message_type = "group"
+      data.group_id = `lark_${eventData.chat_id}`
+      data.group_name = eventData.chat_name || data.group_id
+      Bot.makeLog("info", `群消息：[${data.group_name}(${data.group_id}), ${data.sender.nickname}(${data.user_id})] ${data.raw_message}`, data.self_id)
+    } else {
+      data.message_type = "private"
+      Bot.makeLog("info", `私聊消息：[${data.sender.nickname}(${data.user_id})] ${data.raw_message}`, data.self_id)
+    }
+
+    Bot.em(`${data.post_type}.${data.message_type}`, data)
+  }
+
+    async connect(app_id, app_secret) {
+    const client = new lark.Client({
+      appId: app_id,
+      appSecret: app_secret,
+      appType: lark.AppType.SelfBuild,
+      domain: config.proxy ? config.proxy : lark.Domain.Feishu,
+    })
+
+    try {
+      await client.auth.accessToken.v3.getAccessToken()
+      Bot.makeLog("mark", `飞书机器人连接成功`, app_id)
+    } catch (error) {
+      Bot.makeLog("error", `飞书机器人连接失败: ${error.message}`, app_id)
+      return false
+    }
+
+    const id = `lark_${app_id}`
+    Bot[id] = client
+    Bot[id].adapter = this
+    Bot[id].info = { app_id }
+    Bot[id].uin = id
+    Bot[id].nickname = "LarkBot"
+    Bot[id].avatar = ""
+    Bot[id].version = {
+      id: this.id,
+      name: this.name,
+      version: this.version,
+    }
+    Bot[id].stat = { start_time: Date.now() / 1000 }
+    Bot[id].userCache = new Map()
+    Bot[id].chatCache = new Map()
+
+    Bot[id].pickFriend = user_id => this.pickFriend(id, user_id)
+    Bot[id].pickUser = Bot[id].pickFriend
+
+    Bot[id].getFriendArray = () => this.getFriendArray(id)
+    Bot[id].getFriendList = () => this.getFriendList(id)
+    Bot[id].getFriendMap = () => this.getFriendMap(id)
+
+    Bot[id].pickMember = (group_id, user_id) => this.pickMember(id, group_id, user_id)
+    Bot[id].pickGroup = group_id => this.pickGroup(id, group_id)
+
+    Bot[id].getGroupArray = () => this.getGroupArray(id)
+    Bot[id].getGroupList = () => this.getGroupList(id)
+    Bot[id].getGroupMap = () => this.getGroupMap(id)
+    Bot[id].getGroupMemberMap = () => this.getGroupMemberMap(id)
+
+    Object.defineProperty(Bot[id], "fl", { get() { return this.getFriendMap() }})
+    Object.defineProperty(Bot[id], "gl", { get() { return this.getGroupMap() }})
+    Object.defineProperty(Bot[id], "gml", { get() { return this.getGroupMemberMap() }})
+
+    Bot.makeLog("mark", `${this.name}(${this.id}) ${this.version} 已连接`, id)
+    Bot.em(`connect.${id}`, { self_id: id })
+    return true
+  }
+
+  async load() {
+    if (config.app_id && config.app_secret) {
+      await Bot.sleep(1000, this.connect(config.app_id, config.app_secret))
+    }
+  }
+}
+
+Bot.adapter.push(adapter)
+
+export class LarkPlugin extends plugin {
+  constructor() {
+    super({
+      name: "LarkAdapter",
+      dsc: "飞书适配器设置",
+      event: "message",
+      rule: [
+        {
+          reg: "^#[Ll][Aa][Rr][Kk]账号$",
+          fnc: "List",
+          permission: config.permission,
+        },
+        {
+          reg: "^#[Ll][Aa][Rr][Kk]设置.+$",
+          fnc: "Config",
+          permission: config.permission,
+        },
+        {
+          reg: "^#[Ll][Aa][Rr][Kk](代理|Webhook)",
+          fnc: "Proxy",
+          permission: config.permission,
+        }
+      ]
+    })
+  }
+
+  List() {
+    this.reply(`App ID: ${config.app_id}\nWebhook: ${config.webhook_url}`, true)
+  }
+
+  async Config() {
+    const msg = this.e.msg.replace(/^#[Ll][Aa][Rr][Kk]设置/, "").trim()
+    const [key, value] = msg.split(/\s+/)
+    
+    if (key === "appid") {
+      config.app_id = value
+      this.reply(`App ID 已设置，重启后生效`, true)
+    } else if (key === "secret") {
+      config.app_secret = value
+      this.reply(`App Secret 已设置，重启后生效`, true)
+    } else if (key === "encrypt") {
+      config.encrypt_key = value
+      this.reply(`Encrypt Key 已设置，重启后生效`, true)
+    } else if (key === "verify") {
+      config.verification_token = value
+      this.reply(`Verification Token 已设置，重启后生效`, true)
+    } else {
+      this.reply(`未知配置项: ${key}`, true)
+      return false
+    }
+    
+    await configSave()
+  }
+
+  async Proxy() {
+    const msg = this.e.msg.replace(/^#[Ll][Aa][Rr][Kk](代理|Webhook)/, "").trim()
+    
+    if (this.e.msg.match("代理")) {
+      config.proxy = msg
+      this.reply(`代理已${msg ? "设置" : "删除"}，重启后生效`, true)
+    } else {
+      config.webhook_url = msg
+      this.reply(`Webhook 已${msg ? "设置" : "删除"}，重启后生效`, true)
+    }
+    
+    await configSave()
+  }
+}
+
+logger.info(logger.green("- 飞书适配器插件 加载完成"))
---
+++ lark.md
@@ -0,0 +1,564 @@
+# 飞书机器人适配器使用指南
+
+本指南将详细介绍如何从零开始创建飞书机器人，并将其连接到 TRSS-Yunzai 框架。
+
+## 目录
+
+1. [飞书开放平台申请机器人](#1-飞书开放平台申请机器人)
+2. [获取机器人凭证信息](#2-获取机器人凭证信息)
+3. [安装依赖](#3-安装依赖)
+4. [配置 lark.js](#4-配置-larkjs)
+5. [连接到 TRSS-Yunzai](#5-连接到-trss-yunzai)
+6. [使用说明](#6-使用说明)
+7. [常见问题](#7-常见问题)
+
+---
+
+## 1. 飞书开放平台申请机器人
+
+### 1.1 访问飞书开放平台
+
+打开浏览器，访问 [飞书开放平台](https://open.feishu.cn/)
+
+### 1.2 创建企业自建应用
+
+1. 登录飞书开放平台（需要飞书账号）
+2. 点击右上角「创建应用」
+3. 选择「企业自建应用」
+4. 填写应用信息：
+   - **应用名称**：例如 "TRSS-Yunzai Bot"
+   - **应用描述**：例如 "TRSS-Yunzai 框架的飞书机器人适配器"
+   - **应用图标**：上传一个合适的图标
+5. 点击「创建」
+
+### 1.3 配置应用权限
+
+创建成功后，进入应用详情页面，需要配置以下权限：
+
+#### 必需权限
+
+1. **消息权限**
+   - `im:message` - 发送和接收消息
+   - `im:message:group_at_msg` - 接收群组@消息
+   - `im:message:group_at_msg:readonly` - 读取群组@消息
+
+2. **联系人权限**
+   - `contact:user.base:readonly` - 读取用户基本信息
+   - `contact:user.email:readonly` - 读取用户邮箱（可选）
+
+3. **群组权限**
+   - `im:chat` - 读取群组信息
+   - `im:chat:readonly` - 只读群组信息
+
+#### 添加权限步骤
+
+1. 在应用详情页左侧菜单选择「权限管理」
+2. 搜索并勾选上述必需权限
+3. 点击「申请权限」
+4. 按照提示完成权限申请流程
+
+### 1.4 配置事件订阅
+
+1. 在应用详情页左侧菜单选择「事件订阅」
+2. 开启「启用事件订阅」
+3. 配置请求地址（稍后配置，先跳过）
+4. 订阅以下事件：
+   - `im.message.receive_v1` - 接收消息事件
+
+---
+
+## 2. 获取机器人凭证信息
+
+### 2.1 获取 App ID 和 App Secret
+
+1. 在应用详情页左侧菜单选择「凭证与基础信息」
+2. 复制以下信息：
+   - **App ID**：格式如 `cli_xxxxxxxxxxxxxxxx`
+   - **App Secret**：格式如 `xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx`
+
+### 2.2 获取 Encrypt Key 和 Verification Token（可选）
+
+如果需要启用事件加密验证：
+
+1. 在应用详情页左侧菜单选择「事件订阅」
+2. 点击「添加 Encrypt Key」
+3. 系统会自动生成一个 Encrypt Key，复制保存
+4. 在同一页面找到 Verification Token，复制保存
+
+### 2.3 获取 Webhook 地址（可选）
+
+如果使用 Webhook 方式接收消息：
+
+1. 在应用详情页左侧菜单选择「事件订阅」
+2. 配置请求地址为你的服务器地址
+3. 例如：`https://your-domain.com/lark/webhook`
+
+---
+
+## 3. 安装依赖
+
+### 3.1 安装飞书 SDK
+
+在 TRSS-Yunzai 项目根目录下运行：
+
+```bash
+npm install @larksuiteoapi/node-sdk
+```
+
+### 3.2 验证安装
+
+```bash
+npm list @larksuiteoapi/node-sdk
+```
+
+如果显示版本号，说明安装成功。
+
+---
+
+## 4. 配置 lark.js
+
+### 4.1 放置 lark.js 文件
+
+将 `lark.js` 文件放置到 TRSS-Yunzai 的插件目录中：
+
+```
+TRSS-Yunzai/
+├── plugins/
+│   └── example/
+│       └── lark.js
+```
+
+### 4.2 通过命令行配置
+
+启动 TRSS-Yunzai 后，使用以下命令配置：
+
+#### 设置 App ID
+
+```
+#lark设置 appid cli_xxxxxxxxxxxxxxxx
+```
+
+#### 设置 App Secret
+
+```
+#lark设置 secret xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+```
+
+#### 设置 Encrypt Key（可选）
+
+```
+#lark设置 encrypt xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+```
+
+#### 设置 Verification Token（可选）
+
+```
+#lark设置 verify xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+```
+
+#### 设置代理（可选）
+
+```
+#lark代理 http://proxy.example.com:8080
+```
+
+#### 设置 Webhook URL（可选）
+
+```
+#larkWebhook https://your-domain.com/lark/webhook
+```
+
+### 4.3 查看当前配置
+
+```
+#lark账号
+```
+
+### 4.4 配置文件方式（可选）
+
+也可以直接编辑配置文件，通常位于：
+
+```
+TRSS-Yunzai/data/config/Lark.yaml
+```
+
+配置内容：
+
+```yaml
+tips: ""
+permission: "master"
+app_id: "cli_xxxxxxxxxxxxxxxx"
+app_secret: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+encrypt_key: ""
+verification_token: ""
+webhook_url: ""
+proxy: ""
+```
+
+---
+
+## 5. 连接到 TRSS-Yunzai
+
+### 5.1 重启 TRSS-Yunzai
+
+配置完成后，需要重启 TRSS-Yunzai 使配置生效：
+
+```bash
+# 停止当前运行的 Yunzai
+# 然后重新启动
+node app.js
+```
+
+### 5.2 验证连接
+
+启动后，查看控制台日志，应该看到：
+
+```
+- 正在加载飞书适配器插件
+飞书机器人连接成功
+LarkBot(Lark) v1.0.0 已连接
+- 飞书适配器插件 加载完成
+```
+
+如果看到连接失败的错误，请检查：
+- App ID 和 App Secret 是否正确
+- 网络连接是否正常
+- 代理设置是否正确（如果需要）
+
+### 5.3 测试机器人
+
+1. 在飞书中找到你的机器人应用
+2. 将机器人添加到群组或发起私聊
+3. 发送测试消息，机器人应该能够接收
+
+---
+
+## 6. 使用说明
+
+### 6.1 基本消息发送
+
+在 TRSS-Yunzai 的其他插件中，可以使用以下方式发送消息：
+
+#### 发送文本消息
+
+```javascript
+const bot = Bot['lark_cli_xxxxxxxxxxxxxxxx']
+const group = bot.pickGroup('lark_oc_xxxxxxxxxxxxxxxx')
+
+group.sendMsg('Hello, 飞书！')
+```
+
+#### 发送图片消息
+
+```javascript
+group.sendMsg({
+  type: 'image',
+  file: 'path/to/image.jpg'
+})
+```
+
+#### 发送@提及消息
+
+```javascript
+group.sendMsg([
+  { type: 'text', text: 'Hello ' },
+  { type: 'at', qq: 'lark_ou_xxxxxxxxxxxxxxxx' },
+  { type: 'text', text: '！' }
+])
+```
+
+#### 发送@所有人
+
+```javascript
+group.sendMsg([
+  { type: 'at', qq: 'all' },
+  { type: 'text', text: ' 大家好！' }
+])
+```
+
+### 6.2 消息接收
+
+飞书适配器会自动将接收到的消息转换为 TRSS-Yunzai 的标准格式，并触发相应事件：
+
+#### 监听群组消息
+
+```javascript
+export class MyPlugin extends plugin {
+  constructor() {
+    super({
+      name: 'MyPlugin',
+      dsc: '我的插件',
+      event: 'message.group',
+      priority: 5000,
+      rule: [
+        {
+          reg: '^你好',
+          fnc: 'hello'
+        }
+      ]
+    })
+  }
+
+  hello() {
+    this.reply('你好！我是飞书机器人')
+  }
+}
+```
+
+#### 监听私聊消息
+
+```javascript
+export class PrivatePlugin extends plugin {
+  constructor() {
+    super({
+      name: 'PrivatePlugin',
+      dsc: '私聊插件',
+      event: 'message.private',
+      priority: 5000,
+      rule: [
+        {
+          reg: '^帮助',
+          fnc: 'help
+        }
+      ]
+    })
+  }
+
+  help() {
+    this.reply('这是帮助信息')
+  }
+}
+```
+
+### 6.3 获取用户和群组信息
+
+#### 获取用户信息
+
+```javascript
+const bot = Bot['lark_cli_xxxxxxxxxxxxxxxx']
+const friend = bot.pickFriend('lark_ou_xxxxxxxxxxxxxxxx')
+const userInfo = await friend.getInfo()
+
+console.log(userInfo.nickname)
+console.log(userInfo.avatar)
+```
+
+#### 获取群组列表
+
+```javascript
+const bot = Bot['lark_cli_xxxxxxxxxxxxxxxx']
+const groupList = bot.getGroupList()
+
+console.log(groupList)
+```
+
+#### 获取好友列表
+
+```javascript
+const bot = Bot['lark_cli_xxxxxxxxxxxxxxxx']
+const friendList = bot.getFriendList()
+
+console.log(friendList)
+```
+
+### 6.4 消息撤回
+
+```javascript
+const group = bot.pickGroup('lark_oc_xxxxxxxxxxxxxxxx')
+const msg = await group.sendMsg('这是一条临时消息')
+
+await Bot.sleep(3000)
+group.recallMsg(msg.message_id)
+```
+
+---
+
+## 7. 常见问题
+
+### 7.1 连接失败
+
+**问题**：启动后显示"飞书机器人连接失败"
+
+**解决方案**：
+1. 检查 App ID 和 App Secret 是否正确
+2. 检查网络连接是否正常
+3. 如果使用代理，检查代理设置是否正确
+4. 查看控制台错误日志，获取详细错误信息
+
+### 7.2 无法接收消息
+
+**问题**：机器人无法接收飞书消息
+
+**解决方案**：
+1. 检查事件订阅是否正确配置
+2. 检查权限是否已正确申请
+3. 确认机器人已添加到群组或已发起私聊
+4. 检查 Webhook 地址是否可访问（如果使用 Webhook）
+
+### 7.3 无法发送消息
+
+**问题**：机器人无法发送消息
+
+**解决方案**：
+1. 检查机器人是否有发送消息的权限
+2. 检查群组 ID 或用户 ID 是否正确
+3. 查看控制台错误日志，获取详细错误信息
+
+### 7.4 文件上传失败
+
+**问题**：发送图片、视频等文件失败
+
+**解决方案**：
+1. 检查文件路径是否正确
+2. 检查文件大小是否超过限制
+3. 检查网络连接是否正常
+4. 查看控制台错误日志，获取详细错误信息
+
+### 7.5 @提及不生效
+
+**问题**：发送的@提及消息没有效果
+
+**解决方案**：
+1. 确保用户 ID 格式正确（需要 `lark_` 前缀）
+2. 检查机器人是否有@提及的权限
+3. 确认用户在群组中
+
+### 7.6 代理设置不生效
+
+**问题**：设置了代理但仍然无法连接
+
+**解决方案**：
+1. 确认代理地址格式正确
+2. 测试代理是否可访问
+3. 重启 TRSS-Yunzai 使配置生效
+4. 检查代理服务器是否需要认证
+
+### 7.7 权限不足
+
+**问题**：操作时提示权限不足
+
+**解决方案**：
+1. 在飞书开放平台检查权限配置
+2. 重新申请缺失的权限
+3. 确认权限申请已通过审核
+4. 重启 TRSS-Yunzai 使权限生效
+
+---
+
+## 8. 高级配置
+
+### 8.1 多机器人支持
+
+如果需要连接多个飞书机器人，可以修改 `lark.js` 的配置结构：
+
+```javascript
+const { config, configSave } = await makeConfig("Lark", {
+  bots: [
+    {
+      app_id: "cli_xxxxxxxxxxxxxxxx",
+      app_secret: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    },
+    {
+      app_id: "cli_yyyyyyyyyyyyyyyy",
+      app_secret: "yyyyyyyyyyyyyyyyyyyyyyyyyyyy"
+    }
+  ]
+})
+```
+
+### 8.2 自定义消息处理
+
+可以在 `makeMessage` 方法中添加自定义的消息处理逻辑：
+
+```javascript
+makeMessage(data) {
+  data.post_type = "message"
+  data = this.makeMessageArray(data)
+  
+  if (data.user_id === data.self_id) return
+
+  if (data.chat_type === "group") {
+    data.message_type = "group"
+    data:group_id = `lark_${data.chat_id}`
+    data.group_name = data.chat_name || data.group_id
+    
+    Bot.makeLog("info", `群消息：[${data.group_name}(${data.group_id}), ${data.sender.nickname}(${data.user_id})] ${data.raw_message}`, data.self_id)
+  } else {
+    data.message_type = "private"
+    Bot.makeLog("info", `私聊消息：[${data.sender.nickname}(${data.user_id})] ${data.raw_message}`, data.self_id)
+  }
+
+  Bot.em(`${data.post_type}.${data.message_type}`, data)
+  
+  Bot.em(`lark.message`, data)
+}
+```
+
+### 8.3 Webhook 服务器配置
+
+如果需要使用 Webhook 接收消息，需要配置一个 HTTP 服务器：
+
+```javascript
+import express from 'express'
+import bodyParser from 'body-parser'
+
+const app = express()
+app.use(bodyParser.json())
+
+app.post('/lark/webhook', async (req, res) => {
+  const { challenge } = req.body
+  
+  if (challenge) {
+    res.json({ challenge })
+    return
+  }
+  
+  const adapter = Bot.adapter.find(a => a.id === 'Lark')
+  if (adapter) {
+    adapter.makeMessage(req.body)
+  }
+  
+  res.json({ code: 0, msg: 'success' })
+})
+
+app.listen(3000, () => {
+  console.log('Webhook server listening on port 3000')
+})
+```
+
+---
+
+## 9. 参考资源
+
+- [飞书开放平台文档](https://open.feishu.cn/document)
+- [飞书 Node.js SDK](https://www.npmjs.com/package/@larksuiteoapi/node-sdk)
+- [TRSS-Yunzai 框架](https://github.com/TRSS-Yunzai)
+- [飞书机器人开发指南](https://open.feishu.cn/document/ukTMukTMukTM/ucTM5YjL3ETO24yNxkjN)
+
+---
+
+## 10. 更新日志
+
+### v1.0.0 (2024-03-26)
+
+- 初始版本发布
+- 支持基本的消息发送和接收
+- 支持文本、图片、视频、文件等消息类型
+- 支持@提及功能
+- 支持用户和群组管理
+- 支持代理配置
+- 支持命令行管理
+
+---
+
+## 11. 联系与支持
+
+如有问题或建议，请通过以下方式渠道：
+
+- 提交 Issue 到项目仓库
+- 加入 TRSS-Yunzai 社区讨论
+- 查看飞书开放平台文档
+
+---
+
+**祝使用愉快！**
